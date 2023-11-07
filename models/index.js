9838f3e2e0a986b801480d48f90376ed524adaed
nestibry <nestibry@gmail.com>
2023-11-07 08:42:13 -0600

    Index Model updated

--- models/index.js
+++ models/index.js
@@ -4,13 +4,34 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
-
 // Categories have many Products
+Category.hasMany(Product, {
+    foreignKey: 'category_id',
+    onDelete: 'SET NULL'
+});
+
+// Products belongsTo Category
+Product.belongsTo(Category, {
+    foreignKey: 'category_id',
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+    through:{
+        model: ProductTag,
+        unique: false
+    },
+    as:"my_tags"
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+    through:{
+        model: ProductTag,
+        unique: false
+    },
+    as:"my_products"
+});
 
 module.exports = {
   Product,
