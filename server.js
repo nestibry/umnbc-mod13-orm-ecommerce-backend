76428d2687c1c677816f9b186e33258fb063b773
nestibry <nestibry@gmail.com>
2023-11-07 07:18:24 -0600

    moved over remaining files from develop to main folder...updated a few items here and there

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const routes = require('./routes');
 const sequelize = require('./config/connection');
 require('dotenv').config();
 
-const app = express();
 const PORT = process.env.PORT || 3001;
+const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(routes);
